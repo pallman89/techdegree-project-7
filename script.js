c25e674ef21f8a8720a8b6af6ef43561e7d4b98e
Patrick Allman <pallman89@icloud.com>
2019-11-12 10:03:44 -0700

    More updates

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const alert = document.getElementById("alert");
 
 alert.innerHTML = 
 `<div class="alert-banner"> 
-<p> <strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p> 
-<p class="alert-banner-close">X</p> 
+<p> <strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p> <p class="alert-banner-close">X</p> 
 </div>`
 
 
