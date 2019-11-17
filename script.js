7fbd5dbadec017c41911ad31f2c277cc2acd4711
Patrick Allman <pallman89@icloud.com>
2019-11-17 10:05:16 -0700

    update

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // ALERT BANNER
-const alert = document.getElementById("alert");
+const alertBanner = document.getElementById("alert");
 
 
-alert.innerHTML = 
+alertBanner.innerHTML = 
 `<div class="alert-banner"> 
 <p> <strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p> <p class="alert-banner-close">X</p> 
 </div>`
 
 
 
-alert.addEventListener ('click', (e) => {
+alertBanner.addEventListener ('click', (e) => {
     const element = e.target;
     if(element.classList.contains("alert-banner-close")){
-        alert.style.display = 'none';
+        alertBanner.style.display = 'none';
     }
 });
 
